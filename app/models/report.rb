559847fbfaf6e6f3969323524a33ae84b11c4630
Tetsuyuki Okuzono <fe.snow1@gmail.com>
2016-07-12 12:26:29 +0900

    sort columns in admin view

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,12 @@
 class Report < ActiveRecord::Base
+  rails_admin do
+    list do
+      field :guid
+      field :ver
+      field :platform
+      field :process_type
+      field :created_at
+      field :updated_at
+    end
+  end
 end
